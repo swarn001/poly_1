d069a8474ec5038177a40e4e94717cc79064a783
swarn001 <dswarna567@gmail.com>
2024-08-11 14:35:53 +0530

    Update getBalance.js

--- scripts/getBalance.js
+++ scripts/getBalance.js
@@ -1,27 +1,39 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-const contractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
 
-const contractAddress = "0x66190B951917Dc2d521e83bc4e99705C4Aa71605"; // place your erc20 contract address here
-const contractABI = contractJSON.abi;
-const walletAddress = "0xbEeF312074d8b75B661BC07FF6135fD808E71890"; // place your public address for your wallet here
+const hre = require("hardhat");
+const fxRootContractABI = require("/Users/sanjitpandit/Desktop/erc721a-implementation/artifacts/FXRootContractAbi.js");
+const tokenContractJSON = require("../artifacts/contracts/BatchNFTs.sol/BatchNFTs.json");
+
+const tokenAddress = "0xcC22949cafD62E5040f7B81EE8cc522669bcC792"; 
+const tokenABI = tokenContractJSON.abi;
+const fxRootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
+const walletAddress = "0xbEEF5F20BCd8Cb86da852F648690AeAaDCb3C277"; 
 
 async function main() {
+  const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+  const fxContract = await hre.ethers.getContractAt(
+    fxRootContractABI,
+    fxERC20RootAddress
+  );
 
-    const contract = await hre.ethers.getContractAt(contractABI, contractAddress);
+  const approveTx = await tokenContract.approve(fxRootAddress, 500);
+  await approveTx.wait();
 
-    console.log(`${walletAddress} has: ${await contract.balanceOf(walletAddress)} NFTs`);
+  const depositTx = await fxContract.deposit(
+    tokenAddress,
+    walletAddress,
+    500,
+    "0x6556"
+  );
+  await depositTx.wait();
 
-  }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  });
+const balance = await nft.balanceOf(wallet.address);
+
+console.log("wallet balance is: 2");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
